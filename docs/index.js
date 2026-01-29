dd88c173bc421d54d530e788ddab23318d59d2fc
sjhan <sjhan@motiondynamics.co.kr>
2026-01-29 10:58:59 +0900

    Revert "Merge pull request #1 from HSangJin/V2.0"
    This reverts commit f727a96c5ca5b6801b88ba09c4da3fb5bb1b7f8e, reversing
    changes made to 343695e40a7b6b00be6b9bcbf0049f02e697eaf9.
    
    # Conflicts:
    #	.gitignore
    #	docs/Icon/MD_Resource/MD_logo_350.ico
    #	docs/Icon/MD_Resource/MD_logo_350.png
    #	docs/index.html
    #	docs/index.js

---
+++ docs/index.js
@@ -0,0 +1,30 @@
+function goto_home() {
+    document.querySelector("#home").scrollIntoView({behavior : 'smooth'});
+}
+
+function goto_profile() {
+    document.querySelector("#profile").scrollIntoView({behavior : 'smooth'});
+}
+
+function goto_service() {
+    document.querySelector("#service").scrollIntoView({behavior : 'smooth'});
+}
+
+function goto_tech() {
+    document.querySelector("#technology").scrollIntoView({behavior : 'smooth'});
+}
+
+function goto_center() {
+    document.querySelector("#center").scrollIntoView({behavior : 'smooth'});
+}
+
+function download_catalog() {
+    window.location.href = 'catalog.pdf';
+}
+
+window.addEventListener('keydown', function (e) {
+    if ((e.ctrlKey || e.metaKey) && (e.key === 's' || e.key === 'u' || e.key === 'p')) {
+      e.preventDefault();
+      return false;
+    }
+  });
